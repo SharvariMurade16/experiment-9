7ff0ee4751ce417145c9f120af93c11648ec6474
SharvariMurade16 <sharvari.murade.btech2023@sitpune.edu.in>
2024-08-31 15:57:37 +0530

    Create Call By Value.cpp

---
+++ Call By Value.cpp
@@ -0,0 +1,21 @@
+//Sharvari Murade
+//23070123086
+#include <iostream>
+using namespace std;
+void swap (int a,int b)
+{
+    int temp;
+    temp=a;
+    a=b;
+    b=temp;
+    cout<<"Inside swap a: "<<a<<" b: "<<b<<endl;
+}
+
+int main()
+{
+   int a=11,b=22;
+   cout<<"Before swap a: "<<a<<" b: "<<b<<endl;
+   swap(a,b);
+   cout<<"After swap a:"<<a<<" b:"<<b<<endl;
+    return 0;
+}
